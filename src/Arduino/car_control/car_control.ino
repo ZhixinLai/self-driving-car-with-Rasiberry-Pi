40da8b11ec07fa486b01392d6629f1a6d3554ffe
ZhixinLai <laizhixin16@gmail.com>
2019-12-01 19:24:16 -0500

    traffic_light_obstacle

--- src/Arduino/car_control/car_control.ino
+++ src/Arduino/car_control/car_control.ino
@@ -1,4 +1,6 @@
-    
+int i =0;
+unsigned long int j =0;
+   
 const int EnableL = 5;
 const int HighL = 6;       // LEFT SIDE MOTOR
 const int LowL =7;
@@ -15,21 +17,23 @@ const int D3 = 3;       //Raspberry pin 24    MSB
 int a,b,c,d,data;
 
 
-void setup() 
-{
-  pinMode(EnableL, OUTPUT);
-  pinMode(HighL, OUTPUT);
-  pinMode(LowL, OUTPUT);
-  
-  
-  pinMode(EnableR, OUTPUT);
-  pinMode(HighR, OUTPUT);
-  pinMode(LowR, OUTPUT);
-  
-  pinMode(D0, INPUT_PULLUP);
-  pinMode(D1, INPUT_PULLUP);
-  pinMode(D2, INPUT_PULLUP);
-  pinMode(D3, INPUT_PULLUP);
+void setup() {
+
+pinMode(EnableL, OUTPUT);
+pinMode(HighL, OUTPUT);
+pinMode(LowL, OUTPUT);
+
+
+pinMode(EnableR, OUTPUT);
+pinMode(HighR, OUTPUT);
+pinMode(LowR, OUTPUT);
+
+pinMode(D0, INPUT_PULLUP);
+pinMode(D1, INPUT_PULLUP);
+pinMode(D2, INPUT_PULLUP);
+pinMode(D3, INPUT_PULLUP);
+
+
 }
 
 void Data()
@@ -214,53 +218,221 @@ void UTurn()
 }
 
 
+void Object()
+{
+
+  analogWrite(EnableL, 0);
+  analogWrite(EnableR, 0);            //stop
+  delay(1000);
+
+  digitalWrite(HighL, HIGH);
+  digitalWrite(LowL, LOW);
+  digitalWrite(HighR, LOW);
+  digitalWrite(LowR, HIGH);        //left
+  analogWrite(EnableL, 250);
+  analogWrite(EnableR, 250);
+  delay(500);
+
+  analogWrite(EnableL, 0);
+  analogWrite(EnableR, 0);            //stop
+  delay(200);
+
+  digitalWrite(HighL, LOW);
+  digitalWrite(LowL, HIGH);           //forward
+  digitalWrite(HighR, LOW);
+  digitalWrite(LowR, HIGH);
+  analogWrite(EnableL, 255);
+  analogWrite(EnableR, 255);
+  delay(1000);
+
+  analogWrite(EnableL, 0);           //stop
+  analogWrite(EnableR, 0);
+  delay(200);
+
+  digitalWrite(HighL, LOW);
+  digitalWrite(LowL, HIGH);
+  digitalWrite(HighR, HIGH);         //right
+  digitalWrite(LowR, LOW);
+  analogWrite(EnableL, 255);
+  analogWrite(EnableR, 255);
+  delay(500);
+
+  analogWrite(EnableL, 0);               //stop
+  analogWrite(EnableR, 0);
+  delay(1000);
+
+  
+  digitalWrite(HighL, LOW);
+  digitalWrite(LowL, HIGH);
+  digitalWrite(HighR, LOW);       // forward
+  digitalWrite(LowR, HIGH);
+  analogWrite(EnableL, 150);
+  analogWrite(EnableR, 150);
+  delay(500);
+
+   i  = i+1;
+}
+
+void Lane_Change()
+{
+
+  analogWrite(EnableL, 0);
+  analogWrite(EnableR, 0);            //stop
+  delay(1000);
+
+  digitalWrite(HighL, LOW);
+  digitalWrite(LowL, HIGH);
+  digitalWrite(HighR, HIGH);
+  digitalWrite(LowR, LOW);        //Right
+  analogWrite(EnableL, 250);
+  analogWrite(EnableR, 250);
+  delay(500);
+
+  analogWrite(EnableL, 0);
+  analogWrite(EnableR, 0);            //stop
+  delay(200);
+
+  digitalWrite(HighL, LOW);
+  digitalWrite(LowL, HIGH);           //forward
+  digitalWrite(HighR, LOW);
+  digitalWrite(LowR, HIGH);
+  analogWrite(EnableL, 255);
+  analogWrite(EnableR, 255);
+  delay(800);
+
+  analogWrite(EnableL, 0);           //stop
+  analogWrite(EnableR, 0);
+  delay(200);
+
+  digitalWrite(HighL, HIGH);
+  digitalWrite(LowL, LOW);
+  digitalWrite(HighR, LOW);         //LEFT
+  digitalWrite(LowR, HIGH);
+  analogWrite(EnableL, 255);
+  analogWrite(EnableR, 255);
+  delay(500);
+
+  analogWrite(EnableL, 0);               //stop
+  analogWrite(EnableR, 0);
+  delay(1000);
+
+  
+  digitalWrite(HighL, LOW);
+  digitalWrite(LowL, HIGH);
+  digitalWrite(HighR, LOW);       // forward
+  digitalWrite(LowR, HIGH);
+  analogWrite(EnableL, 150);
+  analogWrite(EnableR, 150);
+  delay(500);
+
+
+}
+
 
 void loop() 
 {
+    if (j > 25000)
+    {
+      Lane_Change();
+      i = 0;
+      j = 0;
+    }
+  
   Data();
   if(data==0)
    {
      Forward();
+     if (i>0)
+     {
+      j = j+1;
+     }
    }
    
   else if(data==1)
    {
      Right1();
+          if (i>0)
+     {
+      j = j+1;
+     }
    }
      
   else if(data==2)
    {
      Right2();
+          if (i>0)
+     {
+      j = j+1;
+     }
    }
      
   else if(data==3)
    {
      Right3();
+          if (i>0)
+     {
+      j = j+1;
+     }
    }
      
   else if(data==4)
    {
      Left1();
+          if (i>0)
+     {
+      j = j+1;
+     }
    }
     
   else if(data==5)
    {
      Left2();
+          if (i>0)
+     {
+      j = j+1;
+     }
    }
     
   else if(data==6)
    {
      Left3();
+          if (i>0)
+     {
+      j = j+1;
+     }
    }
-  
-  else if (data==7)
+     
+  else if(data==7)
    {
      UTurn();
    }
-    
-  else if (data>7)
+  
+  else if (data==8)
+   {
+      analogWrite(EnableL, 0);
+      analogWrite(EnableR, 0);
+      delay(4000);
+
+      analogWrite(EnableL, 150);
+      analogWrite(EnableR, 150);
+      delay(1000);
+   }
+
+   else if(data==9)
+   {
+     Object();
+   }
+
+   else if(data==10)
+   {
+      analogWrite(EnableL, 0);
+      analogWrite(EnableR, 0);
+      delay(2000);
+   }
+
+
+     else if(data>10)
    {
      Stop();
    }
-
 }
